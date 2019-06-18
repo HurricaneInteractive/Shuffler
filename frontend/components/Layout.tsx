b2b059fd4ebe2f82bae217327d0c90026fb726d4
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-06-18 21:25:47 +1000

    Minor adjustments

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 const Layout = styled.div`
 	display: grid;
-	grid-template-columns: auto 300px;
+	grid-template-columns: auto 500px;
 	grid-column-gap: 20px;
 `
 
