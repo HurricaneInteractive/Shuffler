deacc9227e75bd1edba23fcb7f01ed2f0489da1e
Adriaan <adriaan@winkingowl.digital>
2019-06-18 20:48:18 +1000

    Initial frontend commit

---
+++ frontend/components/Layout.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const Layout = styled.div`
+	display: grid;
+	grid-template-columns: auto 300px;
+	grid-column-gap: 20px;
+`
+
+export default Layout
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react"
+
+import Layout from "../components/Layout"
+
+export default (): ReactElement => (
+	<Layout>
+		<div>Editor</div>
+		<div>Preview</div>
+	</Layout>
+)
