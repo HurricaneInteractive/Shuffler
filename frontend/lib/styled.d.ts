9f9ea52881023ac7ff7c2c8e063ab7db4906ac0d
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-06-18 21:45:30 +1000

    Updated Default Theme definition

--- frontend/lib/styled.d.ts
+++ frontend/lib/styled.d.ts
@@ -3,7 +3,40 @@ import "styled-components"
 
 // and extend them!
 declare module "styled-components" {
+	interface IColorMapping {
+		100: string
+		500: string
+		900: string
+	}
+
+	declare type SizeScaling = "xs" | "sm" | "md" | "lg" | "xl"
+
+	declare type FontWeightScaling =
+		| "normal"
+		| "light"
+		| "medium"
+		| "bold"
+		| "black"
+
+	declare interface ISizeScalingNumber {
+		[key: SizeScaling]: number
+	}
+
 	export interface IDefaultTheme {
 		fontFamily: string
+		fontSizes: ISizeScalingNumber
+		colors: {
+			transparent: string
+			black: string
+			white: string
+			grey: IColorMapping
+			green: IColorMapping
+			red: IColorMapping
+			blue: IColorMapping
+			purple: IColorMapping
+		}
+		spacing: ISizeScalingNumber
+		breakpoints: ISizeScalingNumber
+		fontWeight: FontWeightScaling
 	}
 }
