c260d911d37d627c26da2ec79bf038415471f8d4
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-06-18 21:25:31 +1000

    Added theme object

---
+++ frontend/theme/theme.ts
@@ -0,0 +1,8 @@
+import { IDefaultTheme } from "styled-components"
+
+const theme: IDefaultTheme = {
+	fontFamily:
+		"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+}
+
+export default theme
