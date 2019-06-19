f9a35d87ec7671f334c05c5819e0e2bcd50f5d76
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-06-19 21:54:46 +1000

    Updated theme definitions and theme object

--- frontend/lib/styled.d.ts
+++ frontend/lib/styled.d.ts
@@ -3,13 +3,19 @@ import "styled-components"
 
 // and extend them!
 declare module "styled-components" {
-	interface IColorMapping {
+	export interface IColorMapping {
 		100: string
 		500: string
 		900: string
 	}
 
-	declare type SizeScaling = "xs" | "sm" | "md" | "lg" | "xl"
+	export declare type MediaQueryTypes = "min" | "max"
+
+	export declare interface IMediaQueryContents {
+		(): string
+	}
+
+	export declare type SizeScaling = "xs" | "sm" | "md" | "lg" | "xl"
 
 	declare type FontWeightScaling =
 		| "normal"
@@ -18,25 +24,30 @@ declare module "styled-components" {
 		| "bold"
 		| "black"
 
-	declare interface ISizeScalingNumber {
-		[key: SizeScaling]: number
-	}
+	export declare type Colors = "grey" | "green" | "red" | "blue" | "purple"
+
+	export declare type ISpacingCombos =
+		| "p"
+		| "pt"
+		| "pr"
+		| "pb"
+		| "pl"
+		| "px"
+		| "py"
+		| "m"
+		| "mt"
+		| "mr"
+		| "mb"
+		| "ml"
+		| "mx"
+		| "my"
 
 	export interface IDefaultTheme {
 		fontFamily: string
-		fontSizes: ISizeScalingNumber
-		colors: {
-			transparent: string
-			black: string
-			white: string
-			grey: IColorMapping
-			green: IColorMapping
-			red: IColorMapping
-			blue: IColorMapping
-			purple: IColorMapping
-		}
-		spacing: ISizeScalingNumber
-		breakpoints: ISizeScalingNumber
-		fontWeight: FontWeightScaling
+		fontSizes: Record<SizeScaling, number>
+		colors: Record<Colors, IColorMapping>
+		spacing: Record<SizeScaling, number>
+		breakpoints: Record<SizeScaling, number>
+		fontWeight: Record<FontWeightScaling, number | string>
 	}
 }
